e88ee342bb9c507ed05cce7a46b5a247568beebe
Marek <borys@mp.home>
2015-09-08 20:50:43 +0200

    Add binary_num

---
+++ recursion/binary_num.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void binaryPrint(int n)
+{
+   if (1 < n)
+   {
+      binaryPrint(n / 2);
+      if ( 1 == (n % 2))
+         printf("1");
+      else
+         printf("0");
+   }
+   else
+   {
+      if (0 == n)
+         printf("0");
+      if (1 == n)
+         printf("1");
+   }
+}
+
+int main (int argc, char * argv[])
+{
+   if (2 != argc)
+   {
+      printf("Usage: binaryPrint number\n");
+      printf("'number' is positive integer number\n");
+      exit(1);
+   }
+
+   int num = strtol(argv[1], NULL, 10);
+
+   if (num < 0)
+   {
+      printf("number needa to be positive\n");
+      exit(1);
+   }
+
+   binaryPrint(num);
+   printf("\n");
+
+   return 0;
+}
